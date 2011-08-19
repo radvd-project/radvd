eefcc524e4a7b1966bd38b694358f2841e602810
reubenhwk <reubenhwk>
2011-08-19 16:37:05 +0000

    adding patch send by Lonnie for cross compiling to uClibc with no ifaddrs.h
    Modified Files:
    	CHANGES gram.y process.c

--- process.c
+++ process.c
@@ -1,5 +1,5 @@
 /*
- *   $Id: process.c,v 1.28 2011/06/07 04:53:42 reubenhwk Exp $
+ *   $Id: process.c,v 1.29 2011/08/19 16:37:05 reubenhwk Exp $
  *
  *   Authors:
  *    Pedro Roque		<roque@di.fc.ul.pt>
@@ -132,12 +132,12 @@ process(struct Interface *ifacel, unsigned char *msg, int len,
 
 	if (icmph->icmp6_type == ND_ROUTER_SOLICIT)
 	{
-		dlog(LOG_WARNING, 4, "received RS from %s", addr_str);
+		dlog(LOG_DEBUG, 2, "received RS from %s", addr_str);
 		process_rs(iface, msg, len, addr);
 	}
 	else if (icmph->icmp6_type == ND_ROUTER_ADVERT)
 	{
-		dlog(LOG_WARNING, 4, "received RA from %s", addr_str);
+		dlog(LOG_DEBUG, 2, "received RA from %s", addr_str);
 		process_ra(iface, msg, len, addr);
 	}
 }
