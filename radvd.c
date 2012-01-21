1ffc39feeda42b00a8d8d9e106e2f9ed651354a2
Reuben Hawkins <reubenhwk@gmail.com>
2012-01-21 00:53:19 -0800

    changing/adding some logging

--- radvd.c
+++ radvd.c
@@ -445,7 +445,7 @@ void main_loop(void)
 				timer_handler(next);
 		}
 		else if ( rc == -1 ) {
-			flog(LOG_ERR, "poll error: %s", strerror(errno));
+			dlog(LOG_INFO, 3, "poll returned early: %s", strerror(errno));
 		}
 
 		if (sigterm_received || sigint_received) {
@@ -455,12 +455,14 @@ void main_loop(void)
 
 		if (sighup_received)
 		{
+			dlog(LOG_INFO, 3, "sig hup received.\n");
 			reload_config();
 			sighup_received = 0;
 		}
 
 		if (sigusr1_received)
 		{
+			dlog(LOG_INFO, 3, "sig usr1 received.\n");
 			reset_prefix_lifetimes();
 			sigusr1_received = 0;
 		}
