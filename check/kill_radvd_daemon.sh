05c2b44a2165e2e00f43503611faa3b0a6a8debc
Reuben <reubenhwk@gmail.com>
2014-01-13 07:58:37 -0600

    adding kill_radvd tests

---
+++ check/kill_radvd_daemon.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -u
+
+. check/common.sh
+
+
+function baseline {
+cat << EOF
+radvd killed
+starup and main radvd PID are different
+wait success
+sending stop adverts
+returning from radvd main
+EOF
+}
+
+
+function output { 
+trap "ip tuntap del dev radvd0 mode tap" SIGINT SIGTERM
+
+ip tuntap add dev radvd0 mode tap
+ip link set radvd0 up 
+
+./radvd -m logfile -l $RADVD_LOG -d 5 &
+
+sleep 1
+
+PID1=$(cat $RADVD_LOG | trim_log | grep "radvd startup PID is " | awk '{print $NF}')
+PID2=$(cat $RADVD_LOG | trim_log | grep "radvd PID is " | awk '{print $NF}')
+PID3=$(cat $RADVD_LOG | trim_log | grep "radvd privsep PID is " | awk '{print $NF}')
+PID4=$(cat `radvd --version 2>&1 | grep "default pidfile" | awk '{print $NF}' | sed 's/"//g'`)
+
+kill $PID4 && echo "radvd killed" || echo "couldn't kill $PID4"
+
+if [[ "$PID1" == "$PID2" ]] ; then
+	echo "starup and main radvd PID are the same"
+else
+	echo "starup and main radvd PID are different"
+fi
+
+wait $PID1 && echo "wait success" || echo "wait $PID1 failed"
+
+cat $RADVD_LOG | trim_log | grep "sending stop adverts"
+cat $RADVD_LOG | trim_log | grep "returning from radvd main"
+
+ip tuntap del dev radvd0 mode tap
+}
+
+run
