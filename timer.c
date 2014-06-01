845bf6716c69db3e5bd25db7b3d11b03d423cfb4
Reuben Hawkins <reubenhwk@gmail.com>
2014-06-01 18:51:11 -0500

    timer.c: remove unused function: expired

--- timer.c
+++ timer.c
@@ -41,12 +41,3 @@ int next_time_msec(struct Interface const *iface)
 	retval = timevaldiff(&iface->next_multicast, &tv);
 	return retval >= 1 ? retval : 1;
 }
-
-int expired(struct Interface const *iface)
-{
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	if (timevaldiff(&iface->next_multicast, &tv) > 0)
-		return 0;
-	return 1;
-}
