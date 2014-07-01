40866d9d66b1d0450d9bfadeb2af438c849cc354
Reuben Hawkins <reubenhwk@gmail.com>
2014-07-01 17:08:20 -0500

    add test_add_ra_header

--- send.c
+++ send.c
@@ -59,6 +59,25 @@ START_TEST (test_decrement_lifetime)
 }
 END_TEST
 
+START_TEST (test_add_ra_header)
+{
+	struct Interface * ifaces = readin_config("test/test1.conf");
+
+	struct safe_buffer sb = SAFE_BUFFER_INIT;
+	add_ra_header(&sb, &ifaces->ra_header_info, ifaces->state_info.cease_adv);
+
+	unsigned char expected[] = {
+		0x86, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	};
+
+	ck_assert_int_eq(sizeof(expected), sb.used);
+	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sb.used));
+
+	safe_buffer_free(&sb);
+}
+END_TEST
+
 START_TEST (test_add_mtu)
 {
 	struct safe_buffer sb = SAFE_BUFFER_INIT;
@@ -129,6 +148,7 @@ Suite * send_suite(void)
 	tcase_add_test(tc_update, test_decrement_lifetime);
 
 	TCase * tc_build = tcase_create("build");
+	tcase_add_test(tc_build, test_add_ra_header);
 	tcase_add_test(tc_build, test_add_mtu);
 	tcase_add_test(tc_build, test_add_sllao_48);
 	tcase_add_test(tc_build, test_add_sllao_64);
