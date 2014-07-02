7cabff902741e78e8e04e2442b3d479686ed7bb0
Reuben Hawkins <reubenhwk@gmail.com>
2014-07-01 21:37:17 -0500

    send.c: add more unit tests

--- send.c
+++ send.c
@@ -66,7 +66,9 @@ START_TEST (test_add_ra_header)
 
 	struct safe_buffer sb = SAFE_BUFFER_INIT;
 	add_ra_header(&sb, &ifaces->ra_header_info, ifaces->state_info.cease_adv);
-
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
 	unsigned char expected[] = {
 		0x86, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -74,6 +76,7 @@ START_TEST (test_add_ra_header)
 
 	ck_assert_int_eq(sizeof(expected), sb.used);
 	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sb.used));
+#endif
 
 	safe_buffer_free(&sb);
 }
@@ -87,6 +90,11 @@ START_TEST (test_add_prefix)
 	struct safe_buffer sb = SAFE_BUFFER_INIT;
 	add_prefix(&sb, ifaces->AdvPrefixList, ifaces->state_info.cease_adv);
 
+	free_ifaces(ifaces);
+
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
 	unsigned char expected[] = {
 		0x03, 0x04, 0x40, 0xe0, 0xff, 0xff, 0xff, 0xff,
 		0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00,
@@ -104,6 +112,118 @@ START_TEST (test_add_prefix)
 
 	ck_assert_int_eq(sizeof(expected), sb.used);
 	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sb.used));
+#endif
+
+	safe_buffer_free(&sb);
+}
+END_TEST
+
+START_TEST (test_add_route)
+{
+	struct Interface * ifaces = readin_config("test/test1.conf");
+	ck_assert_ptr_ne(0, ifaces);
+
+	struct safe_buffer sb = SAFE_BUFFER_INIT;
+	add_route(&sb, ifaces->AdvRouteList, ifaces->state_info.cease_adv);
+
+	free_ifaces(ifaces);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
+	unsigned char expected[] = {
+		0x18, 0x03, 0x30, 0x18, 0x00, 0x00, 0x27, 0x10,
+		0xfe, 0x80, 0x00, 0x0f, 0x00, 0x01, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x18, 0x03, 0x28, 0x08, 0xff, 0xff, 0xff, 0xff,
+		0xfe, 0x80, 0x00, 0x0f, 0x00, 0x02, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x18, 0x03, 0x20, 0x00, 0x00, 0x00, 0x0b, 0xb8,
+		0xfe, 0x80, 0x00, 0x0f, 0x00, 0x02, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	};
+
+	ck_assert_int_eq(sizeof(expected), sb.used);
+	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sb.used));
+#endif
+	safe_buffer_free(&sb);
+}
+END_TEST
+
+
+START_TEST (test_add_rdnss)
+{
+	struct Interface * ifaces = readin_config("test/test1.conf");
+	ck_assert_ptr_ne(0, ifaces);
+
+	struct safe_buffer sb = SAFE_BUFFER_INIT;
+	add_rdnss(&sb, ifaces->AdvRDNSSList, ifaces->state_info.cease_adv);
+
+	free_ifaces(ifaces);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
+	unsigned char expected[] = {
+		0x19, 0x07, 0x00, 0x00, 0x00, 0x00, 0x04, 0xd2,
+		0xff, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+		0xff, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
+		0xff, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03,
+	};
+
+	ck_assert_int_eq(sizeof(expected), sb.used);
+	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sb.used));
+#endif
+
+	safe_buffer_free(&sb);
+}
+END_TEST
+
+START_TEST (test_add_dnssl)
+{
+	struct Interface * ifaces = readin_config("test/test1.conf");
+	ck_assert_ptr_ne(0, ifaces);
+
+	struct safe_buffer sb = SAFE_BUFFER_INIT;
+	add_dnssl(&sb, ifaces->AdvDNSSLList, ifaces->state_info.cease_adv);
+
+	free_ifaces(ifaces);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
+	unsigned char expected[] = {
+		0x1f, 0x09, 0x00, 0x00, 0x00, 0x00, 0x03, 0xe8,
+		0x06, 0x6f, 0x66, 0x66, 0x69, 0x63, 0x65, 0x06,
+		0x62, 0x72, 0x61, 0x6e, 0x63, 0x68, 0x07, 0x65,
+		0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x63,
+		0x6f, 0x6d, 0x00, 0x06, 0x62, 0x72, 0x61, 0x6e,
+		0x63, 0x68, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70,
+		0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00, 0x07,
+		0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03,
+		0x63, 0x6f, 0x6d, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x1f, 0x09, 0x00, 0x00, 0x00, 0x00, 0x04, 0x4b,
+		0x06, 0x6f, 0x66, 0x66, 0x69, 0x63, 0x65, 0x06,
+		0x62, 0x72, 0x61, 0x6e, 0x63, 0x68, 0x07, 0x65,
+		0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x6e,
+		0x65, 0x74, 0x00, 0x06, 0x62, 0x72, 0x61, 0x6e,
+		0x63, 0x68, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70,
+		0x6c, 0x65, 0x03, 0x6e, 0x65, 0x74, 0x00, 0x07,
+		0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03,
+		0x6e, 0x65, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x1f, 0x09, 0x00, 0x00, 0x00, 0x00, 0x04, 0x4c,
+		0x06, 0x6f, 0x66, 0x66, 0x69, 0x63, 0x65, 0x06,
+		0x62, 0x72, 0x61, 0x6e, 0x63, 0x68, 0x07, 0x65,
+		0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x6f,
+		0x72, 0x67, 0x00, 0x06, 0x62, 0x72, 0x61, 0x6e,
+		0x63, 0x68, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70,
+		0x6c, 0x65, 0x03, 0x6f, 0x72, 0x67, 0x00, 0x07,
+		0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03,
+		0x6f, 0x72, 0x67, 0x00, 0x00, 0x00, 0x00, 0x00,
+	};
+	ck_assert_int_eq(sizeof(expected), sb.used);
+	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sb.used));
+#endif
 
 	safe_buffer_free(&sb);
 }
@@ -112,24 +232,31 @@ END_TEST
 
 START_TEST (test_add_mtu)
 {
+	struct Interface * ifaces = readin_config("test/test1.conf");
+	ck_assert_ptr_ne(0, ifaces);
+
 	struct safe_buffer sb = SAFE_BUFFER_INIT;
+	add_mtu(&sb, ifaces->AdvLinkMTU);
 
-	add_mtu(&sb, 1234);
-
+	free_ifaces(ifaces);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
 	unsigned char expected[] = {
 		0x05, 0x01, 0x00, 0x00, 0x00, 0x00, 0x04, 0xd2,
 	};
 
 	ck_assert_int_eq(sb.used, sizeof(expected));
 	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sizeof(expected)));
+#endif
 
 	safe_buffer_free(&sb);
 }
 END_TEST
 
-START_TEST (test_add_sllao_48)
+START_TEST (test_add_sllao)
 {
-	struct sllao sllao = {
+	struct sllao sllao48 = {
 		{1, 2, 3, 4, 5, 6, 7, 8},
 		48,
 		64,
@@ -137,42 +264,71 @@ START_TEST (test_add_sllao_48)
 	};
 	
 	struct safe_buffer sb = SAFE_BUFFER_INIT;
-	add_sllao(&sb, &sllao);
-
-	unsigned char expected[] = {
+	add_sllao(&sb, &sllao48);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
+	unsigned char expected48[] = {
 		0x01, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
 	};
 
-	ck_assert_int_eq(sizeof(expected), sb.used);
-	ck_assert_int_eq(0, memcmp(sb.buffer, expected, sizeof(expected)));
+	ck_assert_int_eq(sizeof(expected48), sb.used);
+	ck_assert_int_eq(0, memcmp(sb.buffer, expected48, sizeof(expected48)));
+#endif
 
 	safe_buffer_free(&sb);
-}
-END_TEST
 
-START_TEST (test_add_sllao_64)
-{
-	struct sllao sllao = {
+	struct sllao sllao64 = {
 		{1, 2, 3, 4, 5, 6, 7, 8},
 		64,
 		64,
 		1500,
 	};
 	
-	struct safe_buffer sb = SAFE_BUFFER_INIT;
-	add_sllao(&sb, &sllao);
-
-	unsigned char expected[] = {
+	sb = SAFE_BUFFER_INIT;
+	add_sllao(&sb, &sllao64);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
+	unsigned char expected64[] = {
 		0x01, 0x02, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
 		0x07, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	};
 
-	ck_assert_int_eq(sizeof(expected), sb.used);
-	ck_assert_int_eq(0, memcmp(sb.buffer, expected, sizeof(expected)));
+	ck_assert_int_eq(sizeof(expected64), sb.used);
+	ck_assert_int_eq(0, memcmp(sb.buffer, expected64, sizeof(expected64)));
+#endif
 
 	safe_buffer_free(&sb);
 }
 END_TEST
+
+START_TEST (test_add_abro)
+{
+	struct Interface * ifaces = readin_config("test/test1.conf");
+	ck_assert_ptr_ne(0, ifaces);
+
+	struct safe_buffer sb = SAFE_BUFFER_INIT;
+	add_abro(&sb, ifaces->AdvAbroList);
+
+	free_ifaces(ifaces);
+#ifdef PRINT_SAFE_BUFFER
+	print_safe_buffer(&sb);
+#else
+	unsigned char expected[] = {
+		0x23, 0x03, 0x0a, 0x00, 0x02, 0x00, 0x02, 0x00,
+		0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0xa2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+	};
+
+	ck_assert_int_eq(sb.used, sizeof(expected));
+	ck_assert_int_eq(0, memcmp(expected, sb.buffer, sizeof(expected)));
+#endif
+
+	safe_buffer_free(&sb);
+}
+END_TEST
+
 
 Suite * send_suite(void)
 {
@@ -182,9 +338,12 @@ Suite * send_suite(void)
 	TCase * tc_build = tcase_create("build");
 	tcase_add_test(tc_build, test_add_ra_header);
 	tcase_add_test(tc_build, test_add_prefix);
+	tcase_add_test(tc_build, test_add_route);
+	tcase_add_test(tc_build, test_add_rdnss);
+	tcase_add_test(tc_build, test_add_dnssl);
 	tcase_add_test(tc_build, test_add_mtu);
-	tcase_add_test(tc_build, test_add_sllao_48);
-	tcase_add_test(tc_build, test_add_sllao_64);
+	tcase_add_test(tc_build, test_add_sllao);
+	tcase_add_test(tc_build, test_add_abro);
 
 	Suite *s = suite_create("send");
 	suite_add_tcase(s, tc_update);
@@ -749,7 +908,6 @@ static void build_ra(struct safe_buffer * sb, struct Interface const * iface)
 	if (iface->mipv6.AdvHomeAgentInfo
 	    && (iface->mipv6.AdvMobRtrSupportFlag || iface->mipv6.HomeAgentPreference != 0
 		|| iface->mipv6.HomeAgentLifetime != iface->ra_header_info.AdvDefaultLifetime)) {
-
 		add_mipv6_home_agent_info(sb, &iface->mipv6);
 	}
 
