918e26dfbd6dae924e9391b6b56472e9b7c7a11b
Reuben Hawkins <reubenhwk@gmail.com>
2016-03-30 23:30:53 -0700

    netlink.c: cleanup, return -1 on netlink socket open err

--- netlink.c
+++ netlink.c
@@ -34,8 +34,6 @@
 
 void process_netlink_msg(int sock, struct Interface * ifaces)
 {
-	int rc = 0;
-
 	char buf[4096];
 	struct iovec iov = { buf, sizeof(buf) };
 	struct sockaddr_nl sa;
@@ -72,7 +70,6 @@ void process_netlink_msg(int sock, struct Interface * ifaces)
 						dlog(LOG_DEBUG, 3, "netlink: %s, ifindex %d, flags is *NOT* running", ifname,
 						     ifinfo->ifi_index);
 					}
-					++rc;
 				}
 			}
 
@@ -109,8 +106,6 @@ void process_netlink_msg(int sock, struct Interface * ifaces)
 				break;
 			}
 
-			++rc;
-
 			struct Interface *iface = find_iface_by_index(ifaces, ifaddr->ifa_index);
 			if (iface) {
 				touch_iface(iface);
@@ -124,6 +119,7 @@ int netlink_socket(void)
 	int sock = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
 	if (sock == -1) {
 		flog(LOG_ERR, "Unable to open netlink socket: %s", strerror(errno));
+		return -1;
 	}
 #if defined SOL_NETLINK && defined NETLINK_NO_ENOBUFS
 	else if (setsockopt(sock, SOL_NETLINK, NETLINK_NO_ENOBUFS, (int[]){1}, sizeof(int)) < 0) {
